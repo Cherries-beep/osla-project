c2e8917074715e9ec3a9c302744e8f6eff8f5910
Vikaa <viktoriya_vlasova_00@list.ru>
2025-12-10 15:03:37 +0300

    добавлена валидация на имя и колво сотрудников в организации

--- building/models.py
+++ building/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 class Building(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=False)
     entity = models.CharField(max_length=255)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
@@ -21,3 +23,11 @@ class Organization(models.Model):
     name = models.CharField(max_length=255)
     employees_count = models.PositiveIntegerField()
     external_id = models.CharField(max_length=255, unique=True)
+
+    def clean(self):
+
+        if len(str(self.name).strip()) < 2:
+            raise ValidationError({'name': 'Название должно содержать минимум 2 символа.'})
+
+        if int(self.__dict__['employees_count']) > 100000:
+            raise ValidationError({'employees_count': 'Количество сотрудников не может превышать 100000.'})
